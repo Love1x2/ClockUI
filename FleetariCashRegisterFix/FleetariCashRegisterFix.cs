1e348e7412c33f24b57fbca0f9313dc1d840b429
Love1x2 <100617love@gmail.com>
2024-12-14 17:52:47 +0100

    no message

---
+++ FleetariCashRegisterFix/FleetariCashRegisterFix.cs
@@ -0,0 +1,29 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace FleetariCashRegisterFix
+{
+    public class FleetariCashRegisterFix : Mod
+    {
+        public override string ID => "FleetariCashRegisterFix";
+        public override string Name => "FleetariCashRegisterFix";
+        public override string Author => "Love1x2";
+        public override string Version => "1.0";
+        public override string Description => "Fixes the cash register at Fleetari's repair shop.";
+
+        private SphereCollider fleetariRegister;
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.PostLoad, Mod_PostLoad);
+        }
+
+        private void Mod_PostLoad()
+        {
+            // Finds, resizes and offsets the cash register SphereCollider component.
+            fleetariRegister = GameObject.Find("ShopCashRegister/Register").GetComponent<UnityEngine.SphereCollider>();
+            fleetariRegister.radius = 50.0f;
+            fleetariRegister.center = new Vector3(0.0f, 1.0f, 0.0f);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # My Summer Car Mods
 
 ClockUI - A simple digital clock mod for My Summer Car in the style of the original game's UI.
+
+FleetariCashRegisterFix - A basic mod that resizes and offsets the small cash register hitbox at Fleetari's Repair Shop to be more easily accessible.
