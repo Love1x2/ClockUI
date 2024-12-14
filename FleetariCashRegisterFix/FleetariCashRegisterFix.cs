144e4534983a4b43832cbc496f05f624458c3881
Love1x2 <100617love@gmail.com>
2024-12-14 19:27:17 +0100

    no message

--- FleetariCashRegisterFix/FleetariCashRegisterFix.cs
+++ FleetariCashRegisterFix/FleetariCashRegisterFix.cs
@@ -8,22 +8,28 @@ namespace FleetariCashRegisterFix
         public override string ID => "FleetariCashRegisterFix";
         public override string Name => "FleetariCashRegisterFix";
         public override string Author => "Love1x2";
-        public override string Version => "1.0";
+        public override string Version => "1.1";
         public override string Description => "Fixes the cash register at Fleetari's repair shop.";
 
         private SphereCollider fleetariRegister;
 
+        private bool registerFixed = false;
+
         public override void ModSetup()
         {
-            SetupFunction(Setup.PostLoad, Mod_PostLoad);
+            SetupFunction(Setup.Update, Mod_Update);
         }
 
-        private void Mod_PostLoad()
+        private void Mod_Update()
         {
             // Finds, resizes and offsets the cash register SphereCollider component.
-            fleetariRegister = GameObject.Find("ShopCashRegister/Register").GetComponent<UnityEngine.SphereCollider>();
-            fleetariRegister.radius = 50.0f;
-            fleetariRegister.center = new Vector3(0.0f, 1.0f, 0.0f);
+            if (registerFixed == false && GameObject.Find("ShopCashRegister/Register") != null)
+            {
+                fleetariRegister = GameObject.Find("ShopCashRegister/Register").GetComponent<UnityEngine.SphereCollider>();
+                fleetariRegister.radius = 50.0f;
+                fleetariRegister.center = new Vector3(0.0f, 1.0f, 0.0f);
+                registerFixed = true;
+            }
         }
     }
 }
